6d6689e86eb538c8e33b5f0a38363d9a820ad686
lhes23 <32542078+lhes23@users.noreply.github.com>
2022-07-18 14:57:10 +0800

    update the data

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,54 @@ import "./App.css";
 import { IData, sampleData } from "./data";
 import BarChart from "./components/BarChart";
 
+const mySampleData = [
+  {
+    categoryId: "2020",
+    data: [
+      5000, -17000, 15000, 8000, 20000, -600, 13000, 20000, -9000, 4000, 17000,
+      -40000,
+    ],
+  },
+  {
+    categoryId: "2021",
+    data: [
+      -200000, -50000, 3000, 1000, -13000, 338, 3500, 19000, 18000, -5000,
+      17000, 117000,
+    ],
+  },
+  {
+    categoryId: "2022",
+    data: [-70, -26000, -33000, -680, -18000, 1500, 30000],
+  },
+];
+
 function App() {
   const [selectData, setSelectData] = useState<IData>(sampleData[0]);
 
+  const [dataSetData, setDataSetData] = useState<number[]>([]);
+
   const dataHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
-    sampleData.map((data) => {
+    // sampleData.map((data) => {
+    //   if (data.categoryId === e.currentTarget.id) {
+    //     setSelectData({ categoryId: e.currentTarget.id, x: data.x, y: data.y });
+    //   }
+    // });
+
+    mySampleData.map((data) => {
       if (data.categoryId === e.currentTarget.id) {
-        setSelectData({ categoryId: e.currentTarget.id, x: data.x, y: data.y });
+        setDataSetData(data.data);
       }
     });
+    console.log(e.currentTarget.id);
   };
 
-  console.log(selectData);
   return (
     <div className="App">
       <header className="App-header">
         <h1>Interactive Chart</h1>
       </header>
       <div className="btnContainer">
-        {sampleData.map((data) => {
+        {mySampleData.map((data) => {
           return (
             <button
               id={data.categoryId}
@@ -37,7 +66,7 @@ function App() {
       </div>
       <div className="chartContainer">
         <div style={{ width: "50%" }}>
-          <BarChart categoryId="2021" />
+          <BarChart dataSets={dataSetData} />
         </div>
       </div>
     </div>
--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -4,51 +4,18 @@ import { Bar } from "react-chartjs-2";
 import { months, sampleData } from "../data";
 ChartJS.register(...registerables);
 
-const options = {
-  maintainAspectRatio: true,
-  responsive: true,
-};
-
-const BarChart = ({ categoryId }: { categoryId: string }) => {
-  const mySampleData = [
-    {
-      categoryId: "2020",
-      data: [
-        5000, -17000, 15000, 8000, 20000, -600, 13000, 20000, -9000, 4000,
-        17000, -40000,
-      ],
-    },
-    {
-      categoryId: "2021",
-      data: [
-        5000, -17000, 15000, 8000, 20000, -600, 13000, 20000, -9000, 4000,
-        17000, -40000,
-      ],
-    },
-    {
-      categoryId: "2022",
-      data: [
-        5000, -17000, 15000, 8000, 20000, -600, 13000, 20000, -9000, 4000,
-        17000, -40000,
-      ],
-    },
-  ];
-
-  const [dataSetData, setDataSetData] = useState<number[]>([]);
-  useEffect(() => {
-    mySampleData.map((data) => {
-      if (data.categoryId === categoryId) {
-        setDataSetData(data.data);
-      }
-    });
-  }, [categoryId]);
+const BarChart = ({ dataSets }: { dataSets: number[] }) => {
+  const options = {
+    maintainAspectRatio: true,
+    responsive: true,
+  };
 
   const data = {
     labels: months,
     datasets: [
       {
         label: "Cashflow",
-        data: dataSetData,
+        data: dataSets,
         backgroundColor: ["rgba(75, 192, 192, 0.5)"],
         borderColor: ["green"],
         borderWidth: 1,
