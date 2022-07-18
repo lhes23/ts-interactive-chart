4d14cd245a2af4ea73225ffe6919b7a46bd8ff5f
lhes23 <32542078+lhes23@users.noreply.github.com>
2022-07-18 15:00:56 +0800

    put data into data.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,29 +4,8 @@ import "./App.css";
 import { IData, sampleData } from "./data";
 import BarChart from "./components/BarChart";
 
-const mySampleData = [
-  {
-    categoryId: "2020",
-    data: [
-      5000, -17000, 15000, 8000, 20000, -600, 13000, 20000, -9000, 4000, 17000,
-      -40000,
-    ],
-  },
-  {
-    categoryId: "2021",
-    data: [
-      -200000, -50000, 3000, 1000, -13000, 338, 3500, 19000, 18000, -5000,
-      17000, 117000,
-    ],
-  },
-  {
-    categoryId: "2022",
-    data: [-70, -26000, -33000, -680, -18000, 1500, 30000],
-  },
-];
-
 function App() {
-  const [selectData, setSelectData] = useState<IData>(sampleData[0]);
+  // const [selectData, setSelectData] = useState<IData>(sampleData[0]);
 
   const [dataSetData, setDataSetData] = useState<number[]>([]);
 
@@ -37,7 +16,7 @@ function App() {
     //   }
     // });
 
-    mySampleData.map((data) => {
+    sampleData.map((data) => {
       if (data.categoryId === e.currentTarget.id) {
         setDataSetData(data.data);
       }
@@ -51,7 +30,7 @@ function App() {
         <h1>Interactive Chart</h1>
       </header>
       <div className="btnContainer">
-        {mySampleData.map((data) => {
+        {sampleData.map((data) => {
           return (
             <button
               id={data.categoryId}
@@ -65,7 +44,7 @@ function App() {
         })}
       </div>
       <div className="chartContainer">
-        <div style={{ width: "50%" }}>
+        <div style={{ width: "80%" }}>
           <BarChart dataSets={dataSetData} />
         </div>
       </div>
